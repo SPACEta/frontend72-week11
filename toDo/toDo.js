cd2d64901f23ee69805c0db7b7de9a4fe7e060d3
SPACEta <tatyana4aris@mail.ru>
2023-10-24 16:18:37 +0300

    Changed file js

--- toDo/toDo.js
+++ toDo/toDo.js
@@ -1,25 +1,27 @@
+//Находим необходимые элементы DOM и записываем в переменные
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
 
-
-
-const taskList = document.querySelector('.tasklist');
-const noTasks = document.querySelector('.notasks');
-
+//Создаем функцию
 function createTask() {
-    noTasks.classList.add('nodisplay');
-    const taskValue = taskInput.value;
+    //Получаем значение, введенное в поле ввода
+    const taskText = taskInput.value;
+    //Создаем новый элемент списка с помощью метода (createElement)
     const newTask = document.createElement('li');
-    newTask.classList.add('pointer');
-    newTask.classList.add('font');
-    newTask.textContent = taskValue;
+    //Присваиваем текст из поля ввода в качестве содержимого новой задачи (textContent)
+    newTask.textContent = taskText;
+    //Добавляем новую задачу в список с помощью метода (append)
     taskList.append(newTask);
+    //Очищаем поле ввода после добавления задачи
     taskInput.value = '';
 }
 
 function checkTask(newTask) {
-    if (newTask.target.tagName === 'LI') {
+    //Проверяем, что кликнули на элемент списка (event.target)
+    if (newTask.target.tagName === 'li') {
+    //Если условие выполняется, то переключаем состояние выполнения задачи, добавляя или удаляя класс у элемента списка (element.classList.toggle)   
         newTask.target.classList.toggle('checked');
     }
 }
